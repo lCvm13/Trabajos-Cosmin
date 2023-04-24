0ef02fb727d1fc3b9dd4bd6f2b935e4adb546fe4
lCvm13 <70377070+lCvm13@users.noreply.github.com>
2023-04-24 18:43:44 +0200

    Revert "e"
    This reverts commit 9a1e7c0a14f625c174ff3f2469ff8f56dc179aa7.

--- JavaScript/piramide.js
+++ JavaScript/piramide.js
@@ -3,12 +3,6 @@
         let y=3;
         let x=5;
         let s='o';
-        do{
-          var cuadro = prompt('Dime en que cuadro quieres que escriba hola. Valores de 1 al 5','');
-          cuadro= parseInt(cuadro);
-      }while(cuadro<=0||cuadro>5)
-        var selector= "c"+cuadro;
-         var estecuadro= document.getElementById(selector);
         function piramide(){
           for (i=1;i<y+1;i++){
             document.write('<br>');
@@ -18,13 +12,10 @@
                         s+=document.write('o');
                     }
                 }else{
-                    s+=document.write('|');
+                    document.write('|');
                 }
             }
           }
-
-         
+          return s
         }
-    
-        estecuadro.innerHTML=piramide();
        
